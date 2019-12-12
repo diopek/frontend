84e90defcc5237953a09cb2db13f4a95397ccee4
diopek <deha.peker@gmail.com>
2019-12-12 11:28:18 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <p className="App-intro">
+        Finally Worked!!
+        </p>
+    </div>
+  );
+}
+
+export default App;
